c03389b9bc2236b1a26d9792d48658236262faec
hadiburpee <hadiburpee@gmail.com>
2018-11-04 22:05:45 -0800

    spotify and omdb working

--- LIRI.js
+++ LIRI.js
@@ -1,31 +1,93 @@
+//requires the use of dotenv
 require("dotenv").config();
 
 var keys = require("../LIRI/keys");
+var request = require("request");
+var fs = require("fs");
+var userType = 'track';
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var LIRI = process.argv[2];
+var searchQuery = process.argv[3];
+var spotifyObject;
 
-// var spotifyInput1 = process.argv[2];
-var request = require("request");
+//spotify search function
+function spotifySearch(){
+  console.log("Search Query: " + searchQuery);
+  spotify.search({type: userType, query: searchQuery}, function(err, response){
+  // original to write object to text for easy view
+  // spotify.search({ type: 'track', query: searchQuery })
+  //   .then(function(response) {
+  //     fs.writeFile("spotify.txt", JSON.stringify(response, null, 3), 
+  //     function(err){
+        if(err){
+          console.log("Error: " + err);
+        }
+        else{
+          // console.log("Your file was written");
+          spotifyObject = response.tracks.items[0];
+          console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+          + "\n================================"
+          + "\n"
+          + "\nArtist: " + spotifyObject.artists[0].name
+          + "\nSong: " + spotifyObject.name
+          + "\nAlbum: " + spotifyObject.album.name          
+          + "\nLink: " + spotifyObject.external_urls.spotify
+          + "\n"
+          + "\n================================"
+          + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+      });
+}
 
-// var queryUrl = "https://api.spotify.com&apikey=" + spotify;
+function omdbSearch(){
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchQuery 
+    + "&plot=short&apikey=trilogy";
 
-// console.log(queryUrl);
+    request(queryUrl, function(err, response, body){
+      if(err && response.statusCode !== 200){
+        console.log("Error: " + err);
+      }
+      else{
+        var omdbObject = JSON.parse(body);
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
-  console.log("after data");
-  });
+        console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+          + "\n================================"
+          + "\nTitle: " + omdbObject.Title
+          + "\nYear: " + omdbObject.Year
+          + "\nIMDB Rating: " + omdbObject.Ratings[1].value
+          + "\nCountry: " + omdbObject.Country
+          + "\nLanguage: " + omdbObject.Language
+          + "\nPlot: " + omdbObject.Plot
+          + "\nStarring: " + omdbObject.Actors
+          + "\n"
+          + "\n================================"
+          + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      }
+    });
+}
 
-//   spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+//function calls
+if(LIRI === "spotify"){
+  if(searchQuery === undefined){
+    searchQuery = "Ace of Base The Sign";
+    spotifySearch();
+  }
+  else{
+    spotifySearch();
+  }
+}
+if(LIRI === "movie-this"){
+  if(searchQuery === undefined){
+    searchQuery = "Mr. Nobody";
+    omdbSearch();
+  }
+  else{
+    omdbSearch();
+  }
+}
+
+
+
+
 
